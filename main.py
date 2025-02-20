f3ad202ebd87e2129c70c3a3fd109e23f634317f
derwout <wout.deruyck@gmail.com>
2025-02-20 14:23:36 +0100

    added up to CH 11

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 def main():
+    text = get_book_text()
+    lowercase_text = text.lower()
+    # word_count = get_word_count(text)
+    # print(word_count)
+    character_count = count_characters(lowercase_text)
+    alphabet_characters = filter_alphabet(character_count)
+    list_characters(alphabet_characters)
+
+def get_book_text():
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
-    """words = file_contents.split()
-    print(words)"""
+        return f.read()
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    dict = {}
+    for character in text:
+        if character in dict:
+            dict[character] += 1
+        else:
+            dict[character] = 1
+    return dict
+
+def list_characters(character_count):
+    for character in character_count:
+        print(f"The '{character}' character was found {character_count[character]} times")
+
+def filter_alphabet(dict):
+    filtered_dict = {}
+    alphabet = list(map(chr, range(97, 123)))
+    for i in alphabet:
+        if i in dict:
+            filtered_dict[i] = dict[i]
+    return filtered_dict
+
 
 main()
